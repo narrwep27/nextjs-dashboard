a05601f59e7c4491913efb091540757e5955482c
narrwep27 <narrwep@gmail.com>
2023-11-11 14:57:07 -0500

    initial commit; created next app

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { NextFont } from 'next/dist/compiled/@next/font';
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter: NextFont = Inter({ subsets: ['latin'] });
+
+export const lusitana: NextFont = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
